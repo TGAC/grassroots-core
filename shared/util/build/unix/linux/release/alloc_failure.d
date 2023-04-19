4709aba5a1bf2222dae5c28036b215606e30eee8
Simon Tyrrell (EI) <tyrrells@n140943.nbi.ac.uk>
2023-04-19 16:39:48 +0100

    removing object files

--- shared/util/build/unix/linux/release/alloc_failure.d
+++
@@ -1,6 +0,0 @@
-alloc_failure.o: \
- /home/billy/Projects/grassroots/core/shared/util/src/alloc_failure.cpp \
- /home/billy/Projects/grassroots/core/shared/util/include/alloc_failure.hpp
-:
-/home/billy/Projects/grassroots/core/shared/util/src/alloc_failure.cpp:
-/home/billy/Projects/grassroots/core/shared/util/include/alloc_failure.hpp:
